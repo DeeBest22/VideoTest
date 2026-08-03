a4222c147641319f03f30f0568c83332b080cbb9
Abayomi Faith Ayodeji <faithabayomi18@gmail.com>
2026-08-03 16:22:57 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit('user-connected', socket.id);
+
+    socket.on('signal', (data) => {
+      io.to(data.to).emit('signal', { from: socket.id, signal: data.signal });
+    });
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected', socket.id);
+    });
+  });
+});
+
+app.get('/health', (req, res) => res.send('ok'));
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
